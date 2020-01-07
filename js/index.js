c38a0ed669c3d05fb51808737652d31a348a74ab
Nick <nicholasdahl27@gmail.com>
2020-01-07 09:25:11 -0500

    Incoming call modal working

--- js/index.js
+++ js/index.js
@@ -11,6 +11,14 @@ window.addEventListener("load", function() {
   const callButton = document.querySelector("#call-btn");
   const numberInput = document.querySelector("#number-input");
   const readyButton = document.querySelector("#readyButton");
+  const callControlContainer = document.querySelector(
+    "#call-control-container"
+  );
+  const incominCallModal = document.querySelector("#incoming-call-modal");
+  const answerCallButton = document.querySelector("#answer-button");
+  const dismissCallButton = document.querySelector("#dismiss-button");
+
+  let calling = true;
 
   /**
    * Adds a message to the log container in <p> tags
@@ -25,10 +33,26 @@ window.addEventListener("load", function() {
     clientName.innerHTML = "Your client name: <strong>" + name + "</strong>";
   };
 
+  const toggleHangUpButton = () => {
+    console.log("toggled");
+    if (calling) {
+      hangupButton.style.display = "initial";
+      callButton.style.display = "none";
+    } else {
+      hangupButton.style.display = "none";
+      callButton.style.display = "initial";
+    }
+    calling = !calling;
+  };
+
   readyButton.addEventListener("click", () => {
+    readyButton.style.display = "none";
+    callControlContainer.style.display = "inherit";
+    logger.style.display = "inherit";
+
     log("Requesting Capability Token... ");
 
-    const url = "http://a6f77e9d.ngrok.io/twilio/token";
+    const url = "http://3812301a.ngrok.io/twilio/token";
     const params = JSON.stringify({
       authToken: localStorage.getItem("authToken")
     });
@@ -59,21 +83,42 @@ window.addEventListener("load", function() {
 
         Twilio.Device.connect(function(conn) {
           log("Successfully established call!");
-          callButton.style.display = "none";
-          hangupButton.style.display = "initial";
+          toggleHangUpButton();
         });
 
         Twilio.Device.disconnect(function(conn) {
           log("Call ended.");
-          hangupButton.style.display = "none";
-          callButton.style.display = "initial";
+          toggleHangUpButton();
         });
 
         Twilio.Device.incoming(function(conn) {
+          console.log(conn);
           log("Incoming connection from " + conn.parameters.From);
+          setTimeout(() => {
+            $("#incoming-call-modal").modal("toggle");
+          }, 500);
 
-          // accept the incoming connection and start two-way audio
-          conn.accept();
+          function answer(connection, event) {
+            connection.accept();
+          }
+          function reject(connection, event) {
+            connection.reject();
+            $("#incoming-call-modal").modal("toggle");
+            log("Call ended.");
+            //toggleHangUpButton();
+          }
+
+          document
+            .querySelector("#answer-button")
+            .addEventListener("click", answer.bind(event, conn), {
+              once: true
+            });
+
+          document
+            .querySelector("#dismiss-button")
+            .addEventListener("click", reject.bind(event, conn), {
+              once: true
+            });
         });
 
         setClientNameUI(data.identity);
@@ -90,12 +135,13 @@ window.addEventListener("load", function() {
 
     log("Calling " + params.To + "....");
     console.log("Calling " + params.To + "....");
-
+    //toggleHangUpButton();
     Twilio.Device.connect(params);
   });
 
   hangupButton.addEventListener("click", () => {
     log("Hanging up...");
+    //toggleHangUpButton();
     Twilio.Device.disconnectAll();
   });
 });
